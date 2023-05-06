d27fe798b21e5af2e972f0982c90b28786e903ea
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2023-05-06 20:22:08 +0700

    set body rotation

--- mainCode/mainCode.ino
+++ mainCode/mainCode.ino
@@ -2,55 +2,81 @@
 // #include "quassar_movement.h"
 // #include "quassar_gyro.h"
 #include "quassar_compass.h"
-#include "quassar_control.h"
+// #include "quassar_control.h"
 // #include "quassar_communication.h"
 
 /*
   - setup function
    ---------------------------------------------------------------------------*/
-void setup()
-{
-  
-  TIMSK0 = 0;
-  // robot initialization
-  robot_initialization();
-  
-  // initializing acceleration
-  // gyro_setup();
-  
-  // initializing compass
-  compass_setup();
-  
-  // initializing controller
-  // controller_setup();
-  
+void setup() {
+
+    TIMSK0 = 0;
+    // robot initialization
+    robot_initialization();
+
+    // initializing acceleration
+    // gyro_setup();
+
+    // initializing compass
+    compass_setup();
+
+    // initializing controller
+    // controller_setup();
+
+    compass_signal();
+    Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
+    int min_heading = 60;
+    int max_heading = 100;
+    int head = headingDegrees;
+    while (true) {
+        Serial.print("Heading: "); Serial.println(headingDegrees);
+        if (headingDegrees <= min_heading || headingDegrees >= max_heading) {
+            if(headingDegrees <= min_heading) {
+                // turn_right(1, 1, 10);
+                Serial.println("putar kanan");
+                if(headingDegrees <= min_heading || headingDegrees >= max_heading) {
+                    Serial.println("berdiri");
+                    stand();
+                    break;
+                } else {
+                    // continue;
+                }
+            } else {
+//             break;
+            }
+        }
+        // Serial.println("hai");
+        compass_signal();
+    }
 }
 
 /*
   - loop function
    ---------------------------------------------------------------------------*/
+
+// void loop() {}
 void loop()
 {
-  
+
   compass_signal();
   Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
-  
+
   int min_heading = 60;
   int max_heading = 100;
   int head = headingDegrees;
-  
-   
+
+
   // directional set position
     while(headingDegrees <= min_heading || headingDegrees >= max_heading)
     {
-      
+
       if(headingDegrees <= min_heading)
       {
         turn_right(1, 1, 10);
-        Serial.println("putar kanan");
+//         Serial.println("putar kanan");
         if(headingDegrees <= min_heading || headingDegrees >= max_heading)
         {
-          Serial.println("berdiri");
+//           Serial.println("berdiri");
           stand();
           break;
         }
@@ -72,24 +98,24 @@ void loop()
         stand();
         break;
     }
-    
+
     Serial.print("Heading (degrees): "); Serial.println(round(headingDegrees));
-  
+
   delay(500);
-  
+
   // ble_setup();
   delay(2000);
   stand();
-  
+
   // trot_gait_forward(1, 0.25, 10);
-  
+
   // acceleration_show();
-  
+
   // gyro_signal();
-  
-  
+
+
   // // compass_show();
-  
-  
+
+
    // ble_connect();
 }
